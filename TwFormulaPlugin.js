4c8ab719221a569a0552b56a32cf7eb4b6ccb497
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2024-03-15 15:43:53 +0300

    docs: add Author slice, bump version to 0.7.5

--- TwFormulaPlugin.js
+++ TwFormulaPlugin.js
@@ -1,7 +1,8 @@
 /***
 |Name       |TwFormulaPlugin|
 |Description|Render beautiful formulas using LaTeX syntax in wrappers like {{{$$...$$}}}. Plugin supports different libraries for that (MathJax, KaTeX, /%jqMath, %/MathQuill) – the supported LaTeX subset and some features depend on the selected library (MathQuill provides WYSIWYGish editing)|
-|Version    |0.7.4|
+|Version    |0.7.5|
+|Author     |Yakov Litvin|
 |Source     |https://github.com/YakovL/TiddlyWiki_TwFormulaPlugin/blob/master/TwFormulaPlugin.js|
 |Demo       |https://YakovL.github.io/TiddlyWiki_TwFormulaPlugin|
 |Previous contributors|Forked from ~PluginMathJax v1.3, by an anonymous author (called themselves "[[Canada East|http://tiddlywiki.canada-east.ca/]]")/%; jqMath was added thanks to [[this|https://groups.google.com/forum/#!topic/tiddlywiki/PNXaylx1HRY]] thread and the prototype provied by Eric Schulman%/|
